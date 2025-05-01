ee5195804cc72a662bdf4009d96b5d30a87a1e32
Sneha Sharma <devsnehasharma@gmail.com>
2025-05-01 14:16:33 +0530

    Create WebApp.py
    Creates a WebApp to input and store job description and resume file

---
+++ WebApp.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from pypdf import PdfReader
+
+st.set_page_config(page_title="Resume Score Calculator", layout="wide")
+st.markdown(
+    "<h1 style='color: white; text-align: center; font-size: 56px'>Resume Score Calculator</h1>",
+    unsafe_allow_html=True
+)
+st.markdown(
+    "<h5 style='text-align: center; color: grey; font: Times New Roman;'>Instantly get Resume Score based on Job Description and get effective tips on improving your Resume!! </h5>",
+            unsafe_allow_html=True
+    )
+st.markdown(
+    f"""
+    <style>
+    .stApp {{background-color: black;}}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+def read_pdf(file):
+    pdf_reader = PdfReader(file)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text() + "\n"
+    return text
+  
+with st.form('job_desc'):
+	desc = st.text_area(r"$\textsf{\large Enter Job Description: }$")
+	submit_button1 = st.form_submit_button(label='Submit')
+
+if submit_button1:
+  if desc:
+    st.write("Job Description Submitted Successfully.")
+  else:
+    st.write("Please Enter Job Description.")
+
+st.write("\n")
+
+with st.form('resume'):
+  resume_file = st.file_uploader(r"$\textsf{\large Upload Resume: }$", type="pdf")
+  submit_button2 = st.form_submit_button(label='Submit')
+
+if submit_button2:
+  if resume_file:
+    reader = read_pdf(resume_file)
+    st.write("Resume Submitted Succesfully.")
+  else:
+    st.write("Please Upload Resume.")
